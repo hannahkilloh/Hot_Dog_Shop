b881c2447306146b4faa5e968fbe4136da3e6cd8
Hannah Killoh <128159869+hannahkilloh@users.noreply.github.com>
2023-07-23 20:12:14 +0100

    Adding visuals
    Testing visuals of Hotdog shop with a drag Hotdog shop background/graphic

--- shop.py
+++ shop.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class ThreeFailedPayments(Exception):
     pass
 
@@ -13,65 +16,77 @@ menu = {
 available_money = 100
 
 
-def buy_item(item, balance, prices):
-    return item in prices and balance >= prices[item]
+class Shop:
 
+    def run_game(self):
+        pass
 
-def here_is_item_and_goodbye_message(item):
-    print(f"Here's your {item}!\n")
-    print("Thanks for visiting the Hotdog Shop! Have a Nice Day!")
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1259, 859))
+        pygame.display.set_caption("Hotdog shop")
 
+    def buy_item(item, balance, prices):
+        return item in prices and balance >= prices[item]
 
-def retry_payment(item, balance, prices):
-    attempts = 1
-    while attempts <= 3:
-        ask_for_more_money = input(
-            "It looks like you're out of money on your card. Can you add any more money? (Y/N) ")
-        if ask_for_more_money == "Y":
-            # Try for validating additional_money is a float
-            try:
-                additional_money = float(input("How much? (please enter numbers ONLY) "))
-                balance += additional_money
-            # Exception is raised, print error for wrong value type
-            except ValueError:
+    def here_is_item_and_goodbye_message(item):
+        print(f"Here's your {item}!\n")
+        print("Thanks for visiting the Hotdog Shop! Have a Nice Day!")
+
+    def retry_payment(item, balance, prices):
+        attempts = 1
+        while attempts <= 3:
+            ask_for_more_money = input(
+                "It looks like you're out of money on your card. Can you add any more money? (Y/N) ")
+            if ask_for_more_money == "Y":
+                # Try for validating additional_money is a float
+                try:
+                    additional_money = float(input("How much? (please enter numbers ONLY) "))
+                    balance += additional_money
+                # Exception is raised, print error for wrong value type
+                except ValueError:
+                    print("Invalid input. Please try again.")
+
+                if item.buy_item(item, balance, prices):
+                    item.here_is_item_and_goodbye_message(item)
+                    return
+                attempts += 1
+            else:
                 print("Invalid input. Please try again.")
 
-            if buy_item(item, balance, prices):
-                here_is_item_and_goodbye_message(item)
+        raise ThreeFailedPayments()
+
+    def hot_dog_shop(self):
+        print("\nWelcome to The Hotdog Shop!")
+        print("We have many tasty items available. Here's our menu:")
+        for item in menu:
+            print(f"{item} - £{menu.get(item)}")
+
+        user_selection = input(
+            "\nAnything looking tasty? If so, please enter the name of the item you'd like.\nIf not, enter 'exit' to exit the Hotdog Shop.\n")
+
+        try:
+            if user_selection == "exit":
+                print("Thanks for visiting the Hotdog Shop! Have a Nice Day!")
                 return
-            attempts += 1
-        else:
-            print("Invalid input. Please try again.")
-
-    raise ThreeFailedPayments()
-
-
-def hot_dog_shop():
-    print("\nWelcome to The Hotdog Shop!")
-    print("We have many tasty items available. Here's our menu:")
-    for item in menu:
-        print(f"{item} - £{menu.get(item)}")
-
-    user_selection = input(
-        "\nAnything looking tasty? If so, please enter the name of the item you'd like.\nIf not, enter 'exit' to exit the Hotdog Shop.\n")
-
-    try:
-        if user_selection == "exit":
-            print("Thanks for visiting the Hotdog Shop! Have a Nice Day!")
-            return
-        elif user_selection in menu.keys():
-            if buy_item(user_selection, available_money, menu):
-                here_is_item_and_goodbye_message(user_selection)
+            elif user_selection in menu.keys():
+                if self.buy_item(user_selection, available_money, menu):
+                    self.here_is_item_and_goodbye_message(user_selection)
+                else:
+                    self.retry_payment(user_selection, available_money, menu)
             else:
-                retry_payment(user_selection, available_money, menu)
-        else:
-            raise ValueError("Not a valid answer. Please try again.\n")
-    except ValueError as e:
-        print(e)
-        hot_dog_shop()
-    except ThreeFailedPayments as e:
-        print(e)
-        print("Your payment has failed 3 times...please exit the Hotdog Shop. Have a nice day!")
+                raise ValueError("Not a valid answer. Please try again.\n")
+        except ValueError as e:
+            print(e)
+            self.hot_dog_shop()
+        except ThreeFailedPayments as e:
+            print(e)
+            print("Your payment has failed 3 times...please exit the Hotdog Shop. Have a nice day!")
 
 
-hot_dog_shop()
+# hot_dog_shop()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    hds = Shop()
+    hds.run_game()
